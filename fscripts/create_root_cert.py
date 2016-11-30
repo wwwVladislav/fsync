cb640ea8f8a58516739a3bb404e781337079e905
wwwVladislav <wwwVladislav@gmail.com>
2016-11-30 17:03:12 +0300

    Small changes in root certificate generation script

--- fscripts/create_root_cert.py
+++ fscripts/create_root_cert.py
@@ -2,9 +2,9 @@
 import sys, os
 
 def main(argv):
-    p = input("Please enter a password : ")
+    p = input("Please enter a password :")
     os.system("openssl genrsa -des3 -out rsa_key.pem -passout pass:{0} 2048".format(p))
-    os.system("openssl req -x509 -new -key rsa_key.pem -config openssl.cnf -out selfcert.pem -days 10000 -passin pass:{0}".format(p))
+    os.system("openssl req -x509 -new -key rsa_key.pem -config openssl.cnf -out selfcert.crt -days 10000 -passin pass:{0}".format(p))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
